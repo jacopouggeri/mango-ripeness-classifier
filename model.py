45a103f39ef70350998bf6e1b2da92c5504e8e64
mariashapo <72753307+mariashapo@users.noreply.github.com>
2023-10-21 23:21:15 +0100

    Add files via upload
    Model training code and a python script to run the saved model

---
+++ model.py
@@ -0,0 +1,42 @@
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import cv2
+import os
+
+from sklearn.utils import shuffle
+
+import tensorflow as tf
+
+
+# random seeds for reproducibility
+tf.random.set_seed(123)
+
+from tensorflow.keras.models import load_model
+
+loaded_model = load_model('CNN_model.h5')
+
+def hard_predict(array):
+    # The 
+
+    probability = loaded_model.predict(array)
+    
+    # Convert soft probability to hard prediction
+    hard_pred = int(probability >= 0.5)
+
+    return hard_pred
+
+def resize_image(image_array, target_size=(640, 640)):
+
+    # Resizing the image
+    resized_image = cv2.resize(image_array, target_size, interpolation=cv2.INTER_LINEAR)
+    img_with_batch_dimension = np.expand_dims(resized_image, axis=0)
+    return img_with_batch_dimension
+
+def image_process(path):
+    img=cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img=resize_image(img)
+    return img    
+
